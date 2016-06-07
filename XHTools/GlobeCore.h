2c4b858c232967b4598be19d3650ac9d9c263ea7
softgoto <softgoto@gmail.com>
2016-06-07 16:07:01 +0800

    init

---
+++ XHTools/GlobeCore.h
@@ -0,0 +1,106 @@
+//
+//  GlobeCore.h
+//  XHTools
+//
+//  Created by xuhui on 16/6/7.
+//  Copyright © 2016年 softgoto. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//API
+#define Develop_Model   0    // 1 为正式模式  0 为开发模式
+#define API_PATH (Develop_Model) ? @"http://api.sales.mobidocloud.com/rest" : @"http://apidev.sales.mobidocloud.com/rest"
+
+//导航栏高度
+#define NAVIGATION_BAR_HEIGHT    64
+//底部Tabbar高度
+#define TAB_BAR_HEIGHT   49
+
+//是不是iPhone、iPad、Retina屏
+#define IS_IPAD (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad)
+#define IS_IPHONE (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone)
+#define IS_RETINA ([[UIScreen mainScreen] scale] >= 2.0)
+
+//屏幕尺寸
+#define SCREEN_WIDTH ([[UIScreen mainScreen] bounds].size.width)
+#define SCREEN_HEIGHT ([[UIScreen mainScreen] bounds].size.height)
+#define SCREEN_MAX_LENGTH (MAX(SCREEN_WIDTH, SCREEN_HEIGHT))
+#define SCREEN_MIN_LENGTH (MIN(SCREEN_WIDTH, SCREEN_HEIGHT))
+
+//设备
+#define IS_IPHONE_4_OR_LESS (IS_IPHONE && SCREEN_MAX_LENGTH < 568.0)
+#define IS_IPHONE_5 (IS_IPHONE && SCREEN_MAX_LENGTH == 568.0)
+#define IS_IPHONE_6 (IS_IPHONE && SCREEN_MAX_LENGTH == 667.0)
+#define IS_IPHONE_6P (IS_IPHONE && SCREEN_MAX_LENGTH == 736.0)
+
+//版本号
+#define VERSION [[[NSBundle mainBundle] infoDictionary] valueForKey:@"CFBundleShortVersionString"]
+
+//build号
+#define BUILD [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleVersion"]
+
+//Image Name
+#define TTIMAGE(A) [UIImage imageNamed:A]
+
+//Font Size
+#define fontWithBoldSize(size) [UIFont boldSystemFontOfSize:size]
+#define fontWithSize(size) [UIFont systemFontOfSize:size]
+
+//RGB颜色
+#define RGBA(r,g,b,a) [UIColor colorWithRed:(r)/255.0f green:(g)/255.0f blue:(b)/255.0f alpha:(a)]
+#define RGB(r,g,b) RGBA(r, g, b, 1.0f)
+
+//清除背景色
+#define CLEARCOLOR [UIColor clearColor]
+
+//字体大小
+#define FontSize12 fontWithSize(IS_IPHONE_6P?14:12)
+#define FontSize14 fontWithSize(IS_IPHONE_6P?16:14)
+#define FontSize16 fontWithSize(IS_IPHONE_6P?18:16)
+#define FontSize18 fontWithSize(IS_IPHONE_6P?20:18)
+#define FontSize20 fontWithSize(IS_IPHONE_6P?22:20)
+#define FontSize22 fontWithSize(IS_IPHONE_6P?24:22)
+
+#define BoldFontSize12 fontWithBoldSize(IS_IPHONE_6P?14:12)
+#define BoldFontSize14 fontWithBoldSize(IS_IPHONE_6P?16:14)
+#define BoldFontSize16 fontWithBoldSize(IS_IPHONE_6P?18:16)
+
+//重新设定view的Y值
+#define setFrameY(view, newY) view.frame = CGRectMake(view.frame.origin.x, newY, view.frame.size.width, view.frame.size.height)
+#define setFrameX(view, newX) view.frame = CGRectMake(newX, view.frame.origin.y, view.frame.size.width, view.frame.size.height)
+#define setFrameH(view, newH) view.frame = CGRectMake(view.frame.origin.x, view.frame.origin.y, view.frame.size.width, newH)
+
+//取view的坐标及长宽
+#define W(view)    view.frame.size.width
+#define H(view)    view.frame.size.height
+#define X(view)    view.frame.origin.x
+#define Y(view)    view.frame.origin.y
+
+//常用对象 加括号()括起来
+#define APPDELEGATE ((AppDelegate *)[UIApplication sharedApplication].delegate)
+
+//常用对象window
+#define XHKeyWindow [UIApplication sharedApplication].keyWindow
+
+//GBK编码
+#define GBKEncoding CFStringConvertEncodingToNSStringEncoding(kCFStringEncodingGB_18030_2000)
+
+//日志
+#ifdef DEBUG
+#define NSLog(...) NSLog(@"LINE:%d FUNC:%s\n%@\n ",__LINE__,__func__,[NSString stringWithFormat:__VA_ARGS__]);
+#else
+#define NSLog(...)
+#endif
+
+//weakSelf
+#define WeakSelf __weak typeof(self) weakSelf = self;
+
+
+
+
+
+
+
+
+
---
+++ XHTools/GlobeCore.m
@@ -0,0 +1,11 @@
+//
+//  GlobeCore.m
+//  XHTools
+//
+//  Created by xuhui on 16/6/7.
+//  Copyright © 2016年 softgoto. All rights reserved.
+//
+
+#import "GlobeCore.h"
+
+
